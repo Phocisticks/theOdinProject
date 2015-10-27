bcf4827a2132004b38957e80e69fa0ab6d15c426
Mark Glose <bxbitbx@gmail.com>
2015-10-27 12:32:36 -0500

    Grid resizing and color change now work

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
 $(document).ready(function() 
 {
-    var $newdiv = $('<td><div class="square" /></td>');
+    var $block = '<td><div class="square"/></td>';
     var $side = 10;
-   
-    function updateSize(side)
+    var $grid = 250;
+    var $size = 100;
+    function updateSize($side)
     {
+        // Flushes out the previous grid
         $('.row').remove();
+        
+        // Creating the rows
         for (var i = 0; i < $side; i++) 
         {
             $('table').append('<tr class="row"></tr>');   
         }
-        for (var i = 0; i < $side; i++) 
+        // Creating the columns
+        for (var j = 0; j < $side; j++) 
         {
-            $('tr').append($newdiv);  
+            $('tr').append($block);
         }
-    }
+        $('.square').css({"width":"$size"});
+        // Attaches the hover command to the divs
+        // Must be done inside this function otherwise hover won't update after resize
+        $('div').hover( function()
+        {
+           $(this).addClass('selected'); 
+        }); 
+    }      
     
     //Creating the beginning grid
     updateSize($side);
-    
-    // Resizes the grid to user specification
+     // Resizes the grid to user specification
     $('button').click(function()
     {
-        $side = prompt("Choose a side Length");
+        $side = $('input[name=size]').val();
+        $('input[name=size]').val("");
         updateSize($side);
-    });
-    
-    $('.square').hover( function()
-    {
-       $(this).addClass('selected'); 
     });
 });
