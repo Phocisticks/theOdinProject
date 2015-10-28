f4b8c5802555c29fe66404930da5c9c663f16fcc
Phocisticks <bxbitbx@gmail.com>
2015-10-27 20:57:24 -0500

    finished the project, just need to clean up some code

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ $(document).ready(function()
 {
     var $block = '<td><div class="square"/></td>';
     var $side = 10;
-    var $grid = 250;
-    var $size = 100;
+    var $grid = 500;
+    var $size = 35;
+    var $selected = 'option_1'
+    var $color;
+    var $Modifier = 'selected';
+    
+    function whiteout()
+    {
+        $('.square').css({'opacity':'1','background-color':'#FFF'});
+    }
     function updateSize($side)
     {
         // Flushes out the previous grid
@@ -19,22 +27,65 @@ $(document).ready(function()
         {
             $('tr').append($block);
         }
-        $('.square').css({"width":"$size"});
+        // Determines the size of each square
+        $size = $grid / $side;
+        //Changing the size of the squares                
+        $('.square').css({"width":$size,'height':$size});
+        
         // Attaches the hover command to the divs
         // Must be done inside this function otherwise hover won't update after resize
-        $('div').hover( function()
+        function randColor()
         {
-           $(this).addClass('selected'); 
+            return '#'+ Math.floor(Math.random()*16777215).toString(16);
+        }
+        $('.square').hover(function()
+        {
+            switch($selected)
+            {
+                case 'option_1':
+                    $(this).addClass('selected');
+                    break;
+                case 'option_2':
+                    $(this).css("background-color",randColor());
+                    break;
+                case 'option_3':
+                    var opac = $(this).css('opacity');
+            $(this).css('opacity',(opac-0.1));
+                    break;
+                default:
+            }
         }); 
     }      
     
-    //Creating the beginning grid
+    // Creating the beginning grid
     updateSize($side);
-     // Resizes the grid to user specification
+    $('#'+$selected).addClass('choosen');
+    
+    // Resizes the grid to user specification
     $('button').click(function()
     {
         $side = $('input[name=size]').val();
-        $('input[name=size]').val("");
         updateSize($side);
+    });
+    // Adds hover effect to the sidebar selection
+    $('.options').hover(function()
+    {
+        $(this).addClass('active');
+    },
+    function()
+    {
+        $(this).removeClass('active');
+    });
+    
+    $('.options').click(function()
+    {
+        $('.options').removeClass('choosen');
+        $(this).addClass('choosen');
+        $selected = $(this).attr('id');
+        updateSize($side);
+        if($selected == 'option_3')
+        {
+            whiteout();
+        }
     });
 });
